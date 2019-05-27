c3741f5335f8ccd0a7fe2908b2d9fbc92b79fed8
Amar <ahamiltonvincent@gmail.com>
2019-05-27 17:43:47 +1000

    migrate

--- dashboard/migrations/0006_project_category.py
+++ dashboard/migrations/0006_project_category.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='project',
             name='category',
-            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.PROTECT, related_name='projects', to='dashboard.Category'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='projects', to='dashboard.Category', null=True),
             preserve_default=False,
         ),
     ]
